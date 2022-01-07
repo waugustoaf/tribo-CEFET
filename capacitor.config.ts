92b85c054e01aa58719819c527635366526521c9
Augusto Fernandes <waltheraaf@gmail.com>
2022-01-06 22:37:02 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'PROJETO_TRIBO_GRUPO08',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
